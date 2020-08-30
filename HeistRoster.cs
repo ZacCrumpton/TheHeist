da8519bb71fc17f2e3d826df1b7026e4469a3275
zcrumpton <z.crumpton@gmail.com>
2020-08-30 01:09:46 -0500

    team members are saved with required info, team can be displayed when prompted

---
+++ HeistRoster.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+
+namespace TheHeist
+{
+    class HeistRoster
+    {
+        public List<TeamMember> TeamMembers = new List<TeamMember>();
+
+        public void AddTeamMember()
+        {
+            while (true)
+            {
+                Console.WriteLine("Enter a team member's name:");
+                var newTeamMemberName = Console.ReadLine();
+
+                Console.WriteLine("\nWhat is your skill level? (Max Skill Level Is 100):");
+                var newSkillLevel = Convert.ToInt32(Console.ReadLine());
+                if (newSkillLevel <= 100)
+                {
+
+                }
+                else
+                {
+                    while (newSkillLevel > 100)
+                    {
+                        Console.WriteLine("\nSkill Level can be no higher than 100");
+                        newSkillLevel = Convert.ToInt32(Console.ReadLine());
+                    }
+
+                }
+
+                Console.WriteLine("\nWhat is your Courage Factor? (Must Be Between 0.0 and 2.0):");
+                var newCourageFactor = Convert.ToDouble(Console.ReadLine());
+
+
+                var newTeamMember = new TeamMember(newTeamMemberName, newSkillLevel, newCourageFactor);
+                TeamMembers.Add(newTeamMember);
+
+                Console.WriteLine($"\nWelcome to the team: {newTeamMember.Name}");
+
+                Console.WriteLine("\nWould you like to add another heist member? (Enter y or n)");
+
+                var conInput = Console.ReadLine();
+
+                if (conInput == "n")
+                {
+                    Console.WriteLine($"\nYour team is ready for the Heist\n");
+                    break;
+                }
+
+            }
+
+        }
+
+        public void DisplayTeamInfo()
+        {
+            Console.WriteLine($"    >--| Total Members: {TeamMembers.Count} |--<");
+            foreach (var member in TeamMembers)
+            {
+                Console.WriteLine($"        {TeamMembers.IndexOf(member) + 1}. {member.Name} | {member.SkillLevel} | {member.CourageFactor}");
+            }
+        }
+    }
+}
